37112f22a58f09f282a11c45a478954bee372539
Eric Allen <eric.justin.allen@gmail.com>
2013-11-15 20:49:46 -0700

    StringCalculator now accepts custom delimiter
    Heck yes it does.

--- string_calculator.rb
+++ string_calculator.rb
@@ -1,6 +1,16 @@
 class StringCalculator
   def add(value)
-    nums = value.split(/,|\n/)
+    nums = value.split(delimiter(value))
     nums.inject(0) { |sum, n| sum + n.to_i }
+  end
+
+  private
+
+  def delimiter(value)
+    if value.start_with?("//")
+      value[2]
+    else
+      /,|\n/
+    end
   end
 end
--- string_calculator_spec.rb
+++ string_calculator_spec.rb
@@ -31,4 +31,18 @@ describe StringCalculator do
   it 'returns 6 (Fixnum) when passed the string "1\n2,3"' do
     calc.add("1\n2,3").should eq(6)
   end
+
+  xit 'throws an error if passed multiple delimiters together' do
+    expect { calc.add("1,\n2") }.to raise_error
+  end
+
+  describe 'a specified delimiter' do
+    it 'is defined by //[delimeter]\n' do
+      calc.add("//;\n1;2").should eq(3)
+    end
+
+    it 'supports commas' do
+      calc.add("//,\n66,3").should eq(69)
+    end
+  end
 end
