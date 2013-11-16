d4ff80f1921c9193e8aa8b3866343b436510e39b
Eric Allen <eric.justin.allen@gmail.com>
2013-11-15 20:09:37 -0700

    Initial spec file.

---
+++ string_calculator_spec.rb
@@ -0,0 +1,18 @@
+require_relative 'string_calculator'
+require 'rspec'
+
+describe StringCalculator do 
+  let(:calc) { StringCalculator.new }
+
+  it 'returns 0 (Fixnum) when passed an empty string' do
+    calc.add('').should eq(0)
+  end
+
+  it 'returns 1 (Fixnum) when passed the string "1"' do
+    calc.add('1').should eq(1)
+  end
+
+  it 'returns 2 (Fixnum) when passed the string "2"' do
+    calc.add('2').should eq(2)
+  end
+end
