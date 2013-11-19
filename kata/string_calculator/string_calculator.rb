9b0c77739ffa734e0c7184dc21b2816514ab4b54
Eric Allen <eric.justin.allen@gmail.com>
2013-11-19 15:34:45 -0700

    Refactor
    Refactored to include initialize method. By using an initialize method
    and passing our string directly StringCalculator and adding a
    attr_reader I was able to dry up the code and make it much more pleasing
    to read.

--- kata/string_calculator/string_calculator.rb
+++ kata/string_calculator/string_calculator.rb
@@ -1,7 +1,13 @@
 class StringCalculator
-  def add(value)
-    validate(value)
-    prepare_value_array(value).inject(0) do |memo, num|
+  attr_reader :value
+  
+  def initialize(value)
+    @value = value
+    validate
+  end
+  
+  def add
+    prepare_value_array.inject(0) do |memo, num|
       if num.to_i <= 1000
         memo + num.to_i
       else
@@ -13,31 +19,31 @@ class StringCalculator
   private
 
   
-  def validate(value)
-    if negative?(value)
-      raise ArgumentError, "Stop being so negative. Negative numbers were passed: #{collect_negatives(value)}."
-    elsif multiple_delimiters?(value)
+  def validate
+    if negative?
+      raise ArgumentError, "Stop being so negative. Negative numbers were passed: #{collect_negatives}."
+    elsif multiple_delimiters?
       raise ArgumentError, 'Multiple default delimiters not allowed together'
     end
   end
 
-  def negative?(value)
+  def negative?
     true if value.match(/\-\d/) 
   end
 
-  def multiple_delimiters?(value)
+  def multiple_delimiters?
     true if value.match(/[,\n]{2,}/)
   end
 
-  def collect_negatives(value)
-    prepare_value_array(value).select { |num| num.to_i < 0 }.join(', ') 
+  def collect_negatives
+    prepare_value_array.select { |num| num.to_i < 0 }.join(', ') 
   end
 
-  def prepare_value_array(value)
-    value.split(delimiter(value))
+  def prepare_value_array
+    value.split(delimiter)
   end
 
-  def delimiter(value)
+  def delimiter
     if value.start_with?("//")
       value[2]
     else
--- kata/string_calculator/string_calculator_spec.rb
+++ kata/string_calculator/string_calculator_spec.rb
@@ -2,59 +2,69 @@ require_relative 'string_calculator'
 require 'rspec'
 
 describe StringCalculator do 
-  let(:calc) { StringCalculator.new }
 
   it 'returns 0 (Fixnum) when passed an empty string' do
-    calc.add('').should eq(0)
+    string = StringCalculator.new('')
+    string.add.should eq (0)
   end
 
   it 'returns 1 (Fixnum) when passed the string "1"' do
-    calc.add('1').should eq(1)
+    string = StringCalculator.new('1')
+    string.add.should eq (1)
   end
 
   it 'returns 2 (Fixnum) when passed the string "2"' do
-    calc.add('2').should eq(2)
+    string = StringCalculator.new('2')
+    string.add.should eq (2)
   end
 
   it 'returns 3 (Fixnum) when passed the string "1,2"' do
-    calc.add('1,2').should eq(3)
+    string = StringCalculator.new('1,2')
+    string.add.should eq (3)
   end
 
   it 'returns 45 (Fixnum) when passed the string "41,4"' do
-    calc.add('41,4').should eq(45)
+    string = StringCalculator.new('41,4')
+    string.add.should eq (45)
   end
 
   it 'returns 4 (Fixnum) when passed the string "2\n2"' do
-    calc.add("2\n2").should eq(4)
+    string = StringCalculator.new("2\n2")
+    string.add.should eq (4)
   end
 
   it 'returns 6 (Fixnum) when passed the string "1\n2,3"' do
-    calc.add("1\n2,3").should eq(6)
+    string = StringCalculator.new("1\n2,3")
+    string.add.should eq (6)
   end
 
   it 'throws an error if passed multiple delimiters together' do
-    expect { calc.add("1,\n2") }.to raise_error
+    expect { StringCalculator.new("1,\n2") }.to raise_error
   end
 
   describe 'a specified delimiter' do
     it 'is defined by //[delimeter]\n' do
-      calc.add("//;\n1;2").should eq(3)
+      string = StringCalculator.new("//;\n1;2")
+      string.add.should eq (3)
     end
 
     it 'can be any character' do
-      calc.add("//?\n66?3").should eq(69)
+      string = StringCalculator.new("//?\n66?3")
+      string.add.should eq (69)
     end
   end
 
   it 'does not allow negative numbers' do
-    expect { calc.add('-3,3,-1') }.to raise_error(ArgumentError)
+    expect { StringCalculator.new('-3,3,-1') }.to raise_error(ArgumentError)
   end
 
   it 'returns error message with invlaid negative numbers' do 
-    expect { calc.add('-5,3,2,-1,-6') }.to raise_error(ArgumentError, "Stop being so negative. Negative numbers were passed: -5, -1, -6.")
+    expect { StringCalculator.new('-5,3,2,-1,-6') }.to raise_error(ArgumentError, "Stop being so negative. Negative numbers were passed: -5, -1, -6.")
   end
   it 'skips numbers greater than 1000' do
-    expect(calc.add('1000,3')).to eq(1003)
-    expect(calc.add('3,1001')).to eq(3)
+    string = StringCalculator.new('1000,3')
+    string.add.should eq (1003)
+    string = StringCalculator.new('3,1001')
+    string.add.should eq (3)
   end
 end
